04334f72f2346c348e8cb4087c6d5b178a03a7af
Lutfi Lais <74981128+subwayfootlong@users.noreply.github.com>
2025-11-04 15:05:04 +0800

    updated topic

--- Pico1/secrets.h
+++ Pico1/secrets.h
@@ -2,16 +2,16 @@
 #define SECRETS_H
 
 // WiFi Credentials
-#define WIFI_SSID "lutfiesp8266"
-#define WIFI_PASSWORD "ilovesit"
+#define WIFI_SSID "lais14"
+#define WIFI_PASSWORD "deeznuts"
 
 // MQTT Broker settings
-#define MQTT_BROKER_IP "192.168.4.100"
+#define MQTT_BROKER_IP "172.20.10.8"
 #define MQTT_BROKER_PORT 1883
-#define MQTT_CLIENT_ID "PicoTest"
+#define MQTT_CLIENT_ID "Pico1"
 
 // MQTT Topics
-#define TOPIC_CO2 "pico2/co2"        
+#define TOPIC_CO2 "pico2/sensor/data"        
 #define TOPIC_SAFETY_LEVEL "pico3/safety_level"
 //Each Pico should have its own unique topic to avoid message conflicts
 
