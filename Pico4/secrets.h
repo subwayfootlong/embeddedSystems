eba008c4b9656a6ef00d320a140a9494524e7213
Lutfi Lais <74981128+subwayfootlong@users.noreply.github.com>
2025-11-17 19:18:58 +0800

    Integrate TFLite Micro and add model data
    Added tflmicro as a submodule and updated CMakeLists.txt to include and link the pico-tflmicro library. Added model_data.cc containing a TensorFlow Lite model binary. Updated secrets.h with new WiFi and MQTT settings, and added unique MQTT topics for each Pico device.

--- Pico4/secrets.h
+++ Pico4/secrets.h
@@ -1,18 +1,18 @@
 #ifndef SECRETS_H
 #define SECRETS_H
 
-// WiFi Credentials
-#define WIFI_SSID "Lais14"
+#define WIFI_SSID "yourmums"
 #define WIFI_PASSWORD "deeznuts"
 
 // MQTT Broker settings
-#define MQTT_BROKER_IP "172.20.10.8"
+#define MQTT_BROKER_IP "192.168.4.1"
 #define MQTT_BROKER_PORT 1883
-#define MQTT_CLIENT_ID "PicoTest"
+#define MQTT_CLIENT_ID "Pico4"
 
 // MQTT Topics
 #define TOPIC_PUBLISH "test/publish"        
 #define TOPIC_SUBSCRIBE "test/subscribe"
 //Each Pico should have its own unique topic to avoid message conflicts
-
+#define TOPIC_PICO1 "pico1/sensor/data"
+#define TOPIC_PICO2 "pico2/sensor/data"
 #endif
